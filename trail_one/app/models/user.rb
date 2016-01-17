0c8d2df0fb4b7f7f59a6838a074dc7b2075d30b2
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-17 12:34:54 -0800

    Set up all controllers

--- trail_one/app/models/user.rb
+++ trail_one/app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ActiveRecord::Base
-	has_secure_password
-
-	has_many :articles
-
-	def self.confirm(params)
-		@user = User.find_by({email: params[:email]})
-		@user.try(:authenticate, params[:password])
-	end
-
 end
