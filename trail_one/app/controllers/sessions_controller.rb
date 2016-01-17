20962b89916a865b7ed0136254833e50b451ca9f
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-17 12:48:09 -0800

    Added user func

--- trail_one/app/controllers/sessions_controller.rb
+++ trail_one/app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
   def new
+  	@user = User.new
   end
 
   def create
+  	user_params = params.require(:user).permit(:email, :password)
+  	@user = User.confirm(user_params)
+  	if @user
+  		login(@user)
+  		flash[:notice] = "You logged in"
+  		redirect_to @user
+  	else
+  		flash[:error] = "Incorrect email or password"
+  		redirect_to login_path
+  	end
   end
 end
