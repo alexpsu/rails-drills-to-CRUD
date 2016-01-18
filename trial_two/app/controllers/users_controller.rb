b2d99d65b4a8c7d6e123e6445898ea5e0e1c89e6
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-18 12:13:47 -0800

    Made user model

--- trial_two/app/controllers/users_controller.rb
+++ trial_two/app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
+    @user = User.find_by_id(params[:id])
   end
 
   # GET /users/new
@@ -24,17 +21,9 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to @user
   end
 
   # PATCH/PUT /users/1
