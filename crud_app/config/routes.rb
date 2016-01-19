064f376a3f9a8de4e204bbfed5ea4d9a4a5a3659
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-18 22:46:21 -0800

    Can't get any styling to work

--- crud_app/config/routes.rb
+++ crud_app/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :packs
   resources :wolves
   
-  get "/packs/:pack_id/wolves" => "wolf#index", as: "wolves"
+  get "/packs/:pack_id/wolves" => "wolf#index", as: "pack_wolves"
   post "/packs/:pack_id/wolves" => "wolf#create"
 end
