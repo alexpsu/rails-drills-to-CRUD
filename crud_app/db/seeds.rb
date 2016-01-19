c38ce1f0d3f079dbc959e2fd4a690c9c68b3ee82
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2016-01-18 22:37:45 -0800

    Can't get bootstrap to work

--- crud_app/db/seeds.rb
+++ crud_app/db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Pack.destroy_all
+Wolf.destroy_all
+
+mol_params = Hash.new
+  mol_params[:state] = "Wyoming"
+  mol_params[:size] = "12"
+  mol_params[:family_pic]  = "http://news.psu.edu/sites/default/files/styles/photo_gallery_large/public/Mollie's-march-3-2006-b.jpg?itok=SkrNEyzP"
+
+mollies = Pack.create(mol_params)
+
+pup_params = Hash.new
+  pup_params[:state] = "NY"
+  pup_params[:size] = "101"
+  pup_params[:family_pic]  = "http://img1.wikia.nocookie.net/__cb20120411024741/disney/images/a/aa/DalmatianPuppies.jpg"
+
+dalmatians = Pack.create(pup_params)
